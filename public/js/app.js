8636265d98862583a0f81f688691ca3eaafd0b0b
Sneha Morye <morye.sneha@gmail.com>
2021-05-22 19:21:35 +0530

    Remove unneccessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript file is loaded");
-
 /* fetch('http://localhost:3000/weather?address=mumbai').then(response => {
     response.json().then(data => {
         if (data.error) {
